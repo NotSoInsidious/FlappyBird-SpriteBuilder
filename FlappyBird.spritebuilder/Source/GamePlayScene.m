a3c99e34045fa7043fb96640f5b4a8c6622e3f0e
NotSoInsidious <sqixlambeno@hotmail.com>
2014-10-20 15:32:52 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -14,13 +14,26 @@
 -(void)update:(CCTime)delta
 {
     // put update code here
+    //increment the time since the last obstacle was added
+    timeSinceObstacle += delta; //delta is approximately 1/60th of a second
+    
+    //check to see if two seconds have passed
+    if (timeSinceObstacle > 2.0f)
+    {
+        //add a new obstacle
+        [self addObstacle];
+        
+        //the reset timer
+        timeSinceObstacle = 0.0f;
+        
 }
-
+    {
 // put new methods here
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event{
     //this will get called everytime the player touches the screen
     [character flap];
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
     
    }
 
